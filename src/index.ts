89465cfc99f2c2ffed0c31e1cfafc51a60ec00b7
FTWinston <ftwinston@gmail.com>
2021-12-23 13:10:20 +0000

    Fixing generated type of expect

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as mock from 'jest-mock';
 
 export const { jest } = instrument({ jest: mock });
 
-export const { expect } = instrument(
+export const expect: typeof expectPatched = instrument(
   { expect: expectPatched },
   { intercept: (method, path) => path[0] !== 'expect' }
-);
+).expect;

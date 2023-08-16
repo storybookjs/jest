ffdc9e4d3a311beb42b633945f4e40797a2836f3
Kasper Peulen <kasperpeulen@gmail.com>
2023-08-16 17:45:40 +0200

    Make sure that expect matches the runtime type when instrumented

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ import { default as expectPatched } from '@storybook/expect';
 import { instrument } from '@storybook/instrumenter';
 import * as matchers from '@testing-library/jest-dom/matchers';
 import * as mock from 'jest-mock';
-
 const { jest } = instrument({ jest: mock });
 
-const expect: typeof expectPatched = instrument(
+export type Expect = {
+  [P in keyof jest.Expect]: jest.Expect[P];
+} & (<T = any>(
+  actual: T
+) => jest.JestMatchersShape<jest.Matchers<Promise<void>, T>, jest.Matchers<Promise<void>, T>>);
+
+const expect = instrument(
   { expect: expectPatched },
   { intercept: (_method, path) => path[0] !== 'expect' }
-).expect;
+).expect as unknown as Expect;
 
 // @TODO: This should be reverted once https://github.com/testing-library/jest-dom/pull/438 is merged
 // Some bundlers include an undefined `default` in the namespace import,

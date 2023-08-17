b637a39ce935f348b72503bdd8f7166136001ccc
Kasper Peulen <kasperpeulen@gmail.com>
2023-08-17 09:30:21 +0200

    Copy jsdocs, and use interface instead.

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,25 @@ import { default as expectPatched } from '@storybook/expect';
 import { instrument } from '@storybook/instrumenter';
 import * as matchers from '@testing-library/jest-dom/matchers';
 import * as mock from 'jest-mock';
+
 const { jest } = instrument({ jest: mock });
 
-export type Expect = {
-  [P in keyof jest.Expect]: jest.Expect[P];
-} & (<T = any>(
-  actual: T
-) => jest.JestMatchersShape<jest.Matchers<Promise<void>, T>, jest.Matchers<Promise<void>, T>>);
+/**
+ * The `expect` function is used every time you want to test a value.
+ * You will rarely call `expect` by itself.
+ */
+export interface Expect extends Pick<jest.Expect, keyof jest.Expect> {
+  /**
+   * The `expect` function is used every time you want to test a value.
+   * You will rarely call `expect` by itself.
+   *
+   * @param actual The value to apply matchers against.
+   */
+  <T = any>(actual: T): jest.JestMatchersShape<
+    jest.Matchers<Promise<void>, T>,
+    jest.Matchers<Promise<void>, T>
+  >;
+}
 
 const expect = instrument(
   { expect: expectPatched },
